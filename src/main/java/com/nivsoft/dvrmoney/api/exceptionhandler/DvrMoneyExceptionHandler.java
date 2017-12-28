96ed8567528c60e9bde01cf8c3efcfda918a921b
Vinícios A. Rodrigues <viniciosarodrigues@gmail.com>
2017-12-28 10:11:13 -0300

    Adicionando a captura de erros para violação de chaves e integridade de banco.

--- src/main/java/com/nivsoft/dvrmoney/api/exceptionhandler/DvrMoneyExceptionHandler.java
+++ src/main/java/com/nivsoft/dvrmoney/api/exceptionhandler/DvrMoneyExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -39,6 +40,12 @@ public class DvrMoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
 	}
 
+	/**
+	 * Captura erro para buscas de retorno nulo
+	 * @param ex
+	 * @param request
+	 * @return
+	 */
 	@ExceptionHandler({ EmptyResultDataAccessException.class })
 	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex,
 			WebRequest request) {
@@ -47,6 +54,22 @@ public class DvrMoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		String mensagemDesenvolvedor = ex.toString();
 		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
 		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+	}
+
+	/**
+	 * Captura erro de integridade como violação de chaves e constraints
+	 * 
+	 * @param ex
+	 * @param request
+	 * @return
+	 */
+	@ExceptionHandler({ DataIntegrityViolationException.class })
+	public ResponseEntity<Object> handlerDataIntegrityViolationException(DataIntegrityViolationException ex,
+			WebRequest request) {
+		String mensagemUsuario = messageSource.getMessage("", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.toString();
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 
 	/**
