aa5817453c7aab03847f6d0555e822b97634befa
Vinícios A. Rodrigues <viniciosarodrigues@gmail.com>
2018-02-06 16:49:49 -0200

    Serviço de alteração de lançamento adicionado

--- src/main/java/com/nivsoft/dvrmoney/api/resource/LancamentoResource.java
+++ src/main/java/com/nivsoft/dvrmoney/api/resource/LancamentoResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -45,6 +46,14 @@ public class LancamentoResource {
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
 	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable) {
 		return lancamentoService.resumir(lancamentoFilter, pageable);
+	}
+
+	@PutMapping("/{id}")
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO') and #oauth2.hasScope('write')")
+	public ResponseEntity<Lancamento> atualizaLancamento(@PathVariable Long id,
+			@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+		Lancamento lancamentoAlterado = lancamentoService.atualizar(id, lancamento);
+		return ResponseEntity.ok(lancamentoAlterado);
 	}
 
 	@PostMapping
