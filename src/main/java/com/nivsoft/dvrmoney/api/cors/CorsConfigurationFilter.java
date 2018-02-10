bfd26628cf15f8566f237c3e7d9b735bef7a9eac
Vinícios A. Rodrigues <viniciosarodrigues@gmail.com>
2018-02-10 14:06:03 -0200

    Alterando CORS

--- src/main/java/com/nivsoft/dvrmoney/api/cors/CorsConfigurationFilter.java
+++ src/main/java/com/nivsoft/dvrmoney/api/cors/CorsConfigurationFilter.java
@@ -23,7 +23,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class CorsFilter implements Filter {
+public class CorsConfigurationFilter implements Filter {
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -31,7 +31,7 @@ public class CorsFilter implements Filter {
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse resp = (HttpServletResponse) response;
 
-		resp.setHeader("Access-Control-Allow-Origin", "http://10.238.101.68:8000");
+		resp.setHeader("Access-Control-Allow-Origin", "*");
 		resp.setHeader("Access-Control-Allow-Credentials", "true");
 
 		if ("OPTIONS".equals(req.getMethod())) {
